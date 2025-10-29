e9d3d75733f3b51d27ee74deddf34b1b0a4325bb
shivam <shivamnad04@gmail.com>
2025-10-30 01:19:05 +0530

    hiding credentials

--- backend/config/connectMongoDB.js
+++ backend/config/connectMongoDB.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const process = require("process");
-const MONGO_URI ="mongodb+srv://root:root@cluster0.5lam1mo.mongodb.net/test4?retryWrites=true&w=majority&appName=Cluster0";
+const dotenv = require("dotenv");
+dotenv.config();
+const MONGO_URI = process.env.MONGO_URI;
 const connectToMongoDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
